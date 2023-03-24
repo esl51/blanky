67c16c390ca416096081fd5669bd1be9596a06b4
Maxim Zhgulev <max@8max.ru>
2023-03-24 12:08:36 +0300

    header height variable

--- src/scripts/controls/header.js
+++ src/scripts/controls/header.js
@@ -2,6 +2,7 @@ export default class Nav {
   constructor (elem) {
     this.headerElem = elem
     this.scrolledClass = 'is-scrolled'
+    this.resizeTimeout = null
   }
 
   windowScroll () {
@@ -11,18 +12,27 @@ export default class Nav {
     } else if (scrollTop <= 100 && this.headerElem.classList.contains(this.scrolledClass)) {
       this.headerElem.classList.remove(this.scrolledClass)
     }
+    clearTimeout(this.resizeTimeout)
+    this.resizeTimeout = setTimeout(() => {
+      this.setHeight()
+    }, 300)
+  }
+
+  setHeight () {
+    document.documentElement.style.setProperty('--header-height', this.headerElem.offsetHeight + 'px')
   }
 
   mount () {
     this._windowScrollHandler = this.windowScroll.bind(this)
-    this._windowLoadHandler = this.windowScroll.bind(this)
     window.addEventListener('scroll', this._windowScrollHandler)
     window.addEventListener('DOMContentLoaded', this._windowScrollHandler)
+    window.addEventListener('resize', this._windowScrollHandler)
   }
 
   unmount () {
     window.removeEventListener('scroll', this._windowScrollHandler)
     window.removeEventListener('DOMContentLoaded', this._windowScrollHandler)
+    window.removeEventListener('resize', this._windowScrollHandler)
   }
 
   destroy () {
